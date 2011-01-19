055b5a6bafeae75539e63b057ab760064dc29988
Russell Haering <russellhaering@gmail.com>
2011-01-18 23:52:23 -0800

    Improved test script

--- dotest.sh
+++ dotest.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+javac -d bin src/*.java
+
+if type -P colordiff &>/dev/null
+then
+	DIFF=colordiff
+else
+	DIFF=diff
+fi
+
 if [ ! -d "out" ]
 then
 	mkdir out
 fi
 
-for file in $(ls data | grep -v "\.s$")
+for i in 1 2 3 4 5 7 8 9 10 11 m
 do
+	file="test${i}"
+
 	echo "===== TESTING FILE: ${file} ====="
 
 	# Generate our output
-	java -cp bin Asgn2 "data/${file}" > "out/${file}.s"
+	java -cp bin Asgn2 "data/${file}" | sed 's/Reading file data\//Reading file /' > "out/${file}.s"
 
 	# Diff the two
 	colordiff -u "out/${file}.s" "data/${file}.s"
